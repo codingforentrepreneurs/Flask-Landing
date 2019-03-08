408e5d924f56dcfb905f7ccfe61ff71bc63e8f96
Justin Mitchel <hello@teamcfe.com>
2019-03-07 16:20:16 -0800

    14 - Render JSON

--- landing/jobs/views.py
+++ landing/jobs/views.py
@@ -1,7 +1,14 @@
+from flask import jsonify
 from landing import app
+
+# json
+@app.route("/jobs/")
+def jobs_api():
+    data = {'job_id': 123, 'tasks': [12, 13,21312, 3121]}
+    return jsonify(data)
 
 
 @app.route("/jobs/<job_id>/")
 def jobs(job_id):
-    # run job 12
-    return "<h1>Hello {job_id}</h1>".format(job_id=job_id)
+    data = {'job_id': job_id, 'tasks': [12, 13,21312, 3121]}
+    return jsonify(data)
