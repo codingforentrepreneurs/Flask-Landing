3ecb4a0d2758c540f0b6b098067059adf0d0bb6e
Justin Mitchel <hello@teamcfe.com>
2019-03-08 13:19:35 -0800

    17 - Validation & CSRF

--- landing/home/views.py
+++ landing/home/views.py
@@ -1,10 +1,20 @@
+import json
 from flask import render_template, request
 from landing import app
 
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
+    # CSRF -> My app doesn't allow your app to send data
     # http methods
-    print(request.data) # json
-    print(request.form) # html form
+    #print(request.data) # json
+    #print(request.form) # html form
+    if request.method == "POST":
+        # print(request.data)
+        print()
+        data = request.stream.read().decode('utf-8')
+        print(data) 
+        final_data = json.loads(data)
+        print(final_data['hello'])
+
     return render_template('home.html')
