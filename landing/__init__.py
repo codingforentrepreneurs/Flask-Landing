ff80d1549787d2a1f13661941e71ee15b46eb3c9
Justin Mitchel <hello@teamcfe.com>
2019-03-08 13:37:03 -0800

    19 - Adding CSRF Protection to Flask

--- landing/__init__.py
+++ landing/__init__.py
@@ -1,6 +1,20 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
+
 
 app = Flask(__name__)
+csrf = CSRFProtect(app)
+
+# don't share in production
+# secret key 
+# wtf csrf secret key
+
+app.config.update(dict(
+    SECRET_KEY="4b332#@@1!74-006b-4889-8#@#-67b#@#b90ddffd",
+    WTF_CSRF_SECRET_KEY='8207bece0139#4980!af20a681e3bf56f3'
+))
+
+
 
 from .views import *
 from .home.views import *
