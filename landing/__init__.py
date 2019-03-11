2e3407349bf1c2856f5185fff4f4066bf12e3e3f
Justin Mitchel <hello@teamcfe.com>
2019-03-10 17:11:02 -0700

    25 - Setup a Database.

--- landing/__init__.py
+++ landing/__init__.py
@@ -1,6 +1,7 @@
+import os
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 csrf = CSRFProtect(app)
@@ -9,10 +10,16 @@ csrf = CSRFProtect(app)
 # secret key 
 # wtf csrf secret key
 
+db_path = os.path.join(os.path.dirname(__file__), 'db.sqlite3')
+db_uri = 'sqlite:///{}'.format(db_path)
+
 app.config.update(dict(
     SECRET_KEY="4b332#@@1!74-006b-4889-8#@#-67b#@#b90ddffd",
-    WTF_CSRF_SECRET_KEY='8207bece0139#4980!af20a681e3bf56f3'
+    WTF_CSRF_SECRET_KEY='8207bece0139#4980!af20a681e3bf56f3',
+    SQLALCHEMY_DATABASE_URI=db_uri,
 ))
+
+db = SQLAlchemy(app)
 
 
 
