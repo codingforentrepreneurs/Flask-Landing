80f45ba6f6b91e63de0e651791dd56aebc65ac0d
Justin Mitchel <hello@teamcfe.com>
2019-03-10 19:03:07 -0700

    27 - Save to Database

--- landing/home/models.py
+++ landing/home/models.py
@@ -2,7 +2,7 @@ from landing import db
 
 
 class EmailSignup(db.Model):
-    id          = db.Column(db.Integer) # primary_key
+    id          = db.Column(db.Integer, primary_key=True) # primary_key
     full_name   = db.Column(db.String(120), nullable=True)
     email       = db.Column(db.String(120), unique=True, nullable=False)
 
