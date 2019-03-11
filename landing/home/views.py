66f835df39c0b1ce1c686ad4870262c7dc24f457
Justin Mitchel <hello@teamcfe.com>
2019-03-11 12:41:43 -0700

    33 - Database Object to Detail View

--- landing/home/views.py
+++ landing/home/views.py
@@ -1,5 +1,5 @@
 import json
-from flask import render_template, request
+from flask import render_template, request, redirect
 from landing import app
 
 from .forms import LandingForm
@@ -18,11 +18,32 @@ def home():
         if obj is None:
             obj = EmailSignup(**data) #(full_name=, email=)
             obj.save()
-        form = LandingForm()
-        return render_template('home.html', form=form)
+        #form = LandingForm()
+        #return render_template('home.html', form=form)
+        return redirect("/item/{}".format(obj.id))
         # send email -> via flask and smtp
         # create txt doc
         # create csv doc
         # send webhook -> zapier -> google docs
         # save into database -> sqlite -> mysql / postgresql
     return render_template('home.html', form=form)
+
+'''
+create email obj instance via form
+saved in database
+in database, we have objects with ids in models.py
+ids == primary keys
+primary keys to lookup in database
+use dynamic url routing to lookup object in database
+'''
+
+@app.route("/item/<int:id>/", methods=['GET'])
+def item_detail(id):
+    instance = EmailSignup.query.get(id)
+    return render_template('items/detail.html', instance=instance)
+
+
+
+
+
+
