e0475a4a6835e1d5a17a4f232d4fa231319d642a
Justin Mitchel <hello@teamcfe.com>
2019-03-10 19:25:18 -0700

    30 - Convenice Methods for CRUD

--- landing/home/models.py
+++ landing/home/models.py
@@ -6,6 +6,31 @@ class EmailSignup(db.Model):
     full_name   = db.Column(db.String(120), nullable=True)
     email       = db.Column(db.String(120), unique=True, nullable=False)
 
+    def save(self, commit=True):
+        if commit:
+            instance = self
+            if not instance.id:
+                db.session.add(instance)
+            try:
+                db.session.commit()
+            except Exception as e:
+                print("Exception occured\n", e, '\n')
+                db.session.rollback()
+                return False
+            return True
+        return False
+
+    def delete(self, commit=True):
+        if self.id:
+            db.session.delete(self)
+            try:
+                db.session.commit()
+            except Exception as e:
+                print("Exception occured\n", e, '\n')
+                db.session.rollback()
+                return False
+            return True
+        return False
 
 
 # id, full_name, email
