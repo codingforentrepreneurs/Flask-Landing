f85ab67a848a1e933e90dbd6f4f1adad5dad734b
Justin Mitchel <hello@teamcfe.com>
2019-03-10 19:06:19 -0700

    28 - Track Modification Error

--- landing/__init__.py
+++ landing/__init__.py
@@ -17,6 +17,7 @@ app.config.update(dict(
     SECRET_KEY="4b332#@@1!74-006b-4889-8#@#-67b#@#b90ddffd",
     WTF_CSRF_SECRET_KEY='8207bece0139#4980!af20a681e3bf56f3',
     SQLALCHEMY_DATABASE_URI=db_uri,
+    SQLALCHEMY_TRACK_MODIFICATIONS=True,
 ))
 
 db = SQLAlchemy(app)
